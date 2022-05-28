ebedf389a1da51e92a8eb140e61ac59c7283b4c9
Emanuel K <66132515+e1ghts1x@users.noreply.github.com>
2022-05-27 22:43:31 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-    define("IP", "us-cdbr-east-05.cleardb.net");
-    define("USUARIO","b94f1b6d4cf98d");
-    define("SENHA","f4815e7f");
-    define("DATABASE","heroku_20c253997cce061");
+    define("IP", "127.0.0.1");
+    define("USUARIO","root");
+    define("SENHA","");
+    define("DATABASE","form_game2");
     $conectar = mysqli_connect(IP, USUARIO, SENHA, DATABASE) or die ("Não foi possível realizar a conexão");
 ?>
